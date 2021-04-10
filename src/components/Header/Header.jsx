c585563b9495e189a1ea5c84ad117ea64aaa5589
Alexey Polyachinscky <polyachinscky.alex@yandex.ru>
2021-04-10 22:15:28 +0300

    feat: add searchpanel and nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,38 @@ import styles from "./styles.module.css"
 const Header = () => (
   <header className={styles.header}>
     <div className="wrapper">
-      <a href="locolhost:3000">
-        <img className={styles.logo} src="/assets/logo.svg" alt="logo" />
-      </a>
+      <div className={styles.header__main}>
+        <nav className={styles.nav__menu}>
+          <div className={styles.nav__menu_item}>
+            <a href="locolhost:3000">
+              <img className={styles.logo} src="/assets/logo.svg" alt="logo" />
+            </a>
+            <a href="locolhost:3000">Фильмы</a>
+            <a href="locolhost:3000">Сериалы</a>
+            <a href="locolhost:3000">Люди</a>
+            <a href="locolhost:3000">Еще</a>
+          </div>
+          <div className={styles.nav__menu_item}>
+            <a href="locolhost:3000">
+              {/* <img
+                src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-371-plus-white-0bac34f16124808a12ea863b4d9cc6e599dee7c0a80658cfe9ead26939e64517.svg"
+                alt=""
+              /> */}
+              <span className={`${styles.plus} ${styles.logoItem}`} />
+            </a>
+            <a href="locolhost:3000">Язык</a>
+            <a href="locolhost:3000">Войти</a>
+            <a href="locolhost:3000">Присоедениться</a>
+            <a className={styles.search__link} href="locolhost:3000">
+              {/* <img
+                src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-28-search-blue-177462d06db81ff2a02aa022c1c0be5ba4200d7bd3f51091ed9298980e3a26a1.svg"
+                alt=""
+              /> */}
+              <span className={`${styles.search} ${styles.logoItem}`} />
+            </a>
+          </div>
+        </nav>
+      </div>
     </div>
   </header>
 )
--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from "react"
 import PropTypes from "prop-types"
+import styles from "./styles.module.css"
 
 const SearchPanel = ({
   searchMovie,
@@ -12,25 +13,43 @@ const SearchPanel = ({
   useEffect(() => {
     getPopularMovie(1)
     getPopularMovieInTheatre()
-  })
+  }, [])
   const handleSubmit = (e) => {
     e.preventDefault()
     setSearchMovie(e.target.value)
   }
   return (
-    <div className="wrapper">
-      <form>
-        <input
-          type="text"
-          placeholder="Inter film"
-          value={searchMovie}
-          onChange={handleSubmit}
-        />
-        <button type="button" onClick={() => getMovie(searchMovie)}>
-          Search
-        </button>
-      </form>
-    </div>
+    <section className={styles.searchPanel}>
+      <div className="wrapper">
+        <div className={styles.searchPanel__main}>
+          <div className={styles.title}>
+            <h2>Добро пожаловать.</h2>
+            <h3>Миллионы фильмов, сериалов и людей. Исследуйте сейчас.</h3>
+          </div>
+          <form
+            className={styles.formSearch}
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <input
+              className={styles.inputSearch}
+              type="text"
+              placeholder="Inter film"
+              value={searchMovie}
+              onChange={handleSubmit}
+            />
+            <input
+              className={styles.buttonSearch}
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault()
+                getMovie(searchMovie)
+              }}
+              value="Search"
+            />
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 export default SearchPanel
